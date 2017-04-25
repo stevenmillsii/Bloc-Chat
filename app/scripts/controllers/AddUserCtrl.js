b03285690c34ddfb36932488260db4e3032c348b
Steven Mills <samills0608@gmail.com>
2017-04-25 18:28:12 -0400

    almost finished with checkpoint-5

--- app/scripts/controllers/AddUserCtrl.js
+++ app/scripts/controllers/AddUserCtrl.js
@@ -1,12 +1,15 @@
 (function() {
-	function AddUserCtrl($uibModalInstance, $scope) {
+	function AddUserCtrl($uibModalInstance, $scope, $cookies) {
 		$scope.ok = function(currentUser) {
 			$cookies.put('blocChatCurrentUser', currentUser);
-			$uibModalInstance.close('ok');
+			
+			if (currentUser == "") {
+				$scope.warning = "Must Create Username!"
+				} else {$uibModalInstance.close('ok')};
 		};
 	}
 	
 	angular
 		.module('blocChat')
-		.controller('AddUserCtrl', ['$uibModalInstance', '$scope', AddUserCtrl]);
+		.controller('AddUserCtrl', ['$uibModalInstance', '$scope', '$cookies', AddUserCtrl]);
 })();
